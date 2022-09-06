0f0c74355a5cb090d361c8de73c9a71b24e2ba9a
hojncode <glicm12@gmail.com>
2022-09-07 01:07:28 +0900

    Fix(store.js:mbti슬라이스 추가, MbtiForm.jsx: 디스패치&셀렉터 추가+ 쿠키체커&네비게이 터추가+ 불필요한 주석제거 , signUpForm.jsx : 비밀번호 8글자부터 허용가능 ), Add(mbtiSlice.jsx)

---
+++ src/app/modules/mbtiSlice.jsx
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { setCookie } from "../../utils/cookie";
+import instance from "./instance"
+
+
+const initialState = {
+    message: "",
+    errorMessage: "",
+    userInfo: {}
+}
+
+export const postMbtifetch = createAsyncThunk(
+    "mbti/postMbti",
+    async (payload,thunkAPI) => {
+        try {
+            const response = await instance.post("/accounts/mbti", payload);
+            console.log(response)
+            return thunkAPI.fulfillWithValue(response.data)
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.data);
+        }
+    }
+)
+
+const mbtiSlice = createSlice({
+    name: "mbti",
+    initialState,
+    reducers:{},
+    extraReducers: builder => {
+        builder.addCase(postMbtifetch.pending, (state, action) => {
+            state = action.payload;
+            return state;
+        });
+        builder.addCase(postMbtifetch.fulfilled, (state,action) => {
+        const newState = {...state};
+        console.log(newState)
+        newState.message = action.payload.message;
+        setCookie("token", action.payload.token);
+        return newState;
+        });
+        builder.addCase(postMbtifetch.rejected, (state,action) => {
+
+        })
+    }
+})
+
+export default mbtiSlice;
--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import  commentsSlice  from './modules/commentsSlice';
 import accountsSlice from './modules/accountsSlice';
 import mytodosSlice from './modules/mytodosSlice';
+import mbtiSlice from './modules/mbtiSlice';
 
 export const store = configureStore({
   reducer: {
     accounts: accountsSlice.reducer,
     comments:commentsSlice.reducer,
-    mytodos:mytodosSlice.reducer
+    mytodos:mytodosSlice.reducer,
+    mbti:mbtiSlice.reducer,
   },
 });
