200a51b7216c2f39ff6c83aba1150dcb38a78de9
developneoguri <developneoguri@gmail.com>
2022-09-12 02:12:14 +0900

    Fix (helpDesk.jsx: 컨테이너 컴포넌트의 경로가 잘못된 문제 수정, SetToDoContainer.jsx, SetToDoContainer.jsx: 선택 날짜와 현재 날짜 받아오는 변수 선언, 선택 날짜와 오늘 날짜 및 도전 선택 여부에 따라 나오는 컴포넌트가 다르게 적용되도록 수정, 사용하지 않는 스타일드 컴포넌트 변수 제거, HelpDeskContainer.jsx: 컴포넌트의 경로가 잘못된 문제 수정, mytodosSlice.jsx: initialState에 isCompleted key 추가, errorMessage가 제대로 받아와지지 않는 문제 수정, 내가 제안한 도전을 삭제해주는 deleteMyTodosFetch의 Creator와 Reducer 수정, 내가 해보기로 한 도전을 포기시켜주는 deleteSetUpTodoFetch의 Creator와 Reducer 수정, ChallengeCard.jsx: 페이지와 내 상태에 따라 상세 메뉴 버튼을 출력시킬지 여부 관련 설정 로직 추가, 상세 메뉴 누를 시 출력되는 팝업창 기능 추가, 챌린지 진행중/완료 처리를 적용해주는   코드 추가, 오늘 도전하기 위해 등록한 미믹을 취소하거나 내가 제안한 미믹을 삭제할 수 있도록 코드 추가, 기타 추가된 스타일드 컴포넌트 변수 작성 진행)

--- src/pages/helpDesk.jsx
+++ src/pages/helpDesk.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../components/common/footer";
 import Header from "../components/common/header";
-import HelpDeskContainer from "../components/features/helpdesk/HelpDesk";
+import HelpDeskContainer from "../components/features/helpdesk/HelpDeskContainer";
 import Layout from "../layout/layout";
 
 const HelpDesk = () => {
