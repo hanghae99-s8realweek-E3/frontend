d32713a0aecce372243db84a84736bbc813e1df5
developneoguri <developneoguri@gmail.com>
2022-09-14 18:07:57 +0900

    Fix (instance.jsx: 파일이 갱신되지 않아서 토큰 정보가 받아지지 않던 이슈 수정)

--- src/app/modules/instance.jsx
+++ src/app/modules/instance.jsx
@@ -1,23 +1,28 @@
 // axios 모듈을 가져온다.
 import axios from "axios";
 
-
-
 // 기지국을 세워야함 변수명을 instance해서 create를 이용해 기지국을 만들자 (다른거도 가능)
 const instance = axios.create({
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`
-  },
-  baseURL: process.env.REACT_APP_API,
-  withCredentials:true,
-});
-
-export const preInstance = axios.create({
   // headers: {
-  //   Authorization: `Bearer ${myToken}`
+  //   Authorization: `Bearer ${localStorage.getItem("token")}`
   // },
   baseURL: process.env.REACT_APP_API,
-  // withCredentials:true,
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    config.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+  },
+  function (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+export const preInstance = axios.create({
+  baseURL: process.env.REACT_APP_API,
 });
 
 export default instance;
