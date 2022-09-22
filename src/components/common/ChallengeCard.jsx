34294fa555b0cbcee55e29f20e05722d46072e82
developneoguri <developneoguri@gmail.com>
2022-09-23 01:29:03 +0900

    Merge (revision: ) - Fix   - ChallengeCard.jsx: 미 로그인 시에 상세 피드 진입 시, 아무런 경고창이 발생하지 않는 문제 수정

--- src/components/common/ChallengeCard.jsx
+++ src/components/common/ChallengeCard.jsx
@@ -3,18 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage, faStar } from "@fortawesome/free-regular-svg-icons";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { tokenChecker } from "../../utils/token";
 
 function ChallengeCard({ data }) {
   const navigate = useNavigate();
-
   // 상세 피드 페이지로 이동시켜줌.
   function moveToFeedDetail() {
-    if (data.todoInfo.todoId !== "null" && data.todoInfo.todoId !== undefined)
-      navigate(`/feeddetail/${data.todoInfo.todoId}`);
+    if (data.todoInfo.todoId !== "null" && data.todoInfo.todoId !== undefined) {
+      if (tokenChecker() === true) {
+        navigate(`/feeddetail/${data.todoInfo.todoId}`);
+      } else if (tokenChecker() === false) {
+        alert("로그인 후 이용해주세요.");
+        return;
+      }
+    }
   }
-  console.log(data);
-  // 이용 시, <ChallengeCard id={todoId} data={객체값} key={idx} hideState={true/false} isTodayChallenge={true/false} />로 작성해줄 것
-  // map을 쓰지 않는 경우, key는 예외.
+
+  // 이용 시, <ChallengeCard data={객체값} />로 작성해줄 것
   return (
     <StChallengeCardDiv
       background={data.isChallenged === true ? "#DDDDDD" : "#ffffff"}>
