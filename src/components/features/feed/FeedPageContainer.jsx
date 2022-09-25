eb8484cd9585564f1f94565b995a0bd1f347e11b
rlaedous <103303516+rlaedous@users.noreply.github.com>
2022-09-25 21:35:09 +0900

    Fix - ProfileCard.jsx : 궁합알아보기 나오는 위치 수정 - FeedPageContainer.jsx : 검색기능 구현중

--- src/components/features/feed/FeedPageContainer.jsx
+++ src/components/features/feed/FeedPageContainer.jsx
@@ -15,25 +15,47 @@ import {
 import ChallengeCard from "../../common/ChallengeCard";
 import { tokenChecker } from "../../../utils/token";
 import LoadingContainer from "../../../utils/loadingState";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 // check uncheck
 
 function FeedPageContainer() {
   const [selectSort, setSelectSort] = useState(false);
   const [sortState, setSortState] = useState("최신순");
+  const [inputContext, setInputContext] = useState("");
+  const [searchList, setSearchList] = useState([]);
   // 스토어에서 todolists리듀서 호출
   const feedCard = useSelector((state) => state.todolists.data);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   // mbti선택하기를 했을때 mbti를 불러옴
   const { mbti } = useParams();
-
+  console.log(searchList.length);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const inputSearch = (e) => {
+    setInputContext(e.target.value);
+  };
+  const searchData = (e) => {
+    e.preventDefault();
+    let todoData;
+    // 1. 검색어가 빈 값일 때? 그러면 어떻게 처리할 것인가? = 아니면 다 노출시킬 것인지?
+    // console.log(followData)
+    todoData = feedCard.map((x) => x.todoInfo);
+    console.log(todoData);
+    setSearchList(
+      todoData.filter((elem) => elem.todo.indexOf(inputContext) !== -1)
+    );
+    // console.log(searchList)
+    // inputContext === "" ? setPostLIst(data)
+  };
   // 무한 스크롤 때 사용
   // console.log(inView);
   // console.log(card.length);
   // console.log(mbti);
   console.log(feedCard);
+  console.log(feedCard.map((x) => x.todoInfo));
 
   //feedCard.length 이슈 아래 주석 참조 mbti선택후 다른 페이지 이동후 다시 피드페이지 들어왔을 때 선택했던 mbti가 나타남
   // 첫 렌더링시 토큰/토큰x 에 따라 스토어에서 각각 리듀서 실행
@@ -59,8 +81,8 @@ function FeedPageContainer() {
 
   //처음 로딩될때 로그인/미로로그인 mbti의 유무에 따라서 렌더링
   useEffect(() => {
-    console.log("로딩시작")
-    setLoading(true)
+    console.log("로딩시작");
+    setLoading(true);
     if (tokenChecker() === false && mbti === undefined) {
       dispatch(getTodoListsFetch(false));
     } else if (tokenChecker() === false && mbti !== undefined) {
@@ -70,8 +92,8 @@ function FeedPageContainer() {
       dispatch(getTodoListsFetch(true));
     } else if (tokenChecker() === true && mbti !== undefined)
       dispatch(getMbtiTodoListsFetch({ login: true, mbti: mbti }));
-      console.log("로딩끝") 
-      setLoading(false)
+    console.log("로딩끝");
+    setLoading(false);
   }, []);
 
   // 처음에 화면 렌더링될 때는 의미없는 렌더링, mbti 선택후 렌더링될 때 유효함
@@ -84,7 +106,6 @@ function FeedPageContainer() {
       dispatch(getTodoListsFetch(true));
     } else if (mbti !== undefined)
       dispatch(getMbtiTodoListsFetch({ login: true, mbti: mbti }));
-      
   }, [mbti]);
 
   //checkOn의  초기값은 false로 설정
@@ -105,7 +126,7 @@ function FeedPageContainer() {
   //최신순 댓글순 도전순 이미지 및 커서 클릭시 선택한 값에 따라 값 출력  토큰유무-> mbti유무
   // 1. 로그인을 했는지 안했는지 2.로그인을했으면 mbti를 설정했는지 안했는지
   const sortDate = (e) => {
-    setLoading(true)
+    setLoading(true);
     if (tokenChecker() === false) {
       if (mbti === undefined) {
         dispatch(getTodoListsFetch(false));
@@ -121,7 +142,7 @@ function FeedPageContainer() {
         }
       }
     }
-    setLoading(false)
+    setLoading(false);
     setSortState("최신순");
     setSelectSort(!selectSort);
     console.log(e.target.id);
@@ -145,7 +166,7 @@ function FeedPageContainer() {
         }
       }
     }
-    setLoading(false)
+    setLoading(false);
     setSortState("댓글순");
     setSelectSort(!selectSort);
   };
@@ -167,115 +188,146 @@ function FeedPageContainer() {
         }
       }
     }
-    setLoading(false)
+    setLoading(false);
     setSortState("도전순");
     setSelectSort(!selectSort);
   };
   // MBTI 선택 버튼 클릭시
   const moveToSelectMBTI = () => {
-    setLoading(true)
+    setLoading(true);
     navigate("/selectmbtifeed");
   };
+  console.log(searchList?.filter((elem) => elem.isChallenged === false).map((x) => x.todoInfo.todo));
+  // console.log(feedCard?.filter((elem) => elem.isChallenged === true))
+  console.log(searchList?.filter((elem) => elem.isChallenged === false))
+  console.log(searchList)
+  console.log(searchList.todo)
 
+
+  console.log(searchList?.filter((elem) => elem.todoInfo))
   return (
     <>
-    {loading === true ? <LoadingContainer/> : <></>}
-    <StTotalWrap>
-      {selectSort === true ? (
-        <StShadowBackgroundDiv onClick={toggleSortPopUp}>
-          <StPopupBox>
-            <StSlideDiv />
-            <StSort>
-              <StDate
-                style={{
-                  color: sortState === "최신순" ? "#ff6d53" : "#8d8d8d",
-                }}
-                onClick={sortDate}
-              >
-                최신순
-              </StDate>
-              <StDateLine />
-              <StComment
-                style={{
-                  color: sortState === "댓글순" ? "#ff6d53" : "#8d8d8d",
-                }}
-                onClick={sortComment}
-              >
-                댓글순
-              </StComment>
-              <StCommentLine />
-              <StChallenge
-                style={{
-                  color: sortState === "도전순" ? "#ff6d53" : "#8d8d8d",
-                }}
-                onClick={sortChallenge}
-              >
-                도전순
-              </StChallenge>
-              <StChallengeLine />
-              <StCommonBar />
-            </StSort>
-          </StPopupBox>
-        </StShadowBackgroundDiv>
-      ) : (
-        <></>
-      )}
-      <StTopWrap>
-        <StChallengeWrap>
-          {/* 거짓이면 체크안한거 참이면 체크한거 */}
-          {checkOn === false ? (
-            <StChallengeImg
-              onClick={checkState}
-              src={process.env.PUBLIC_URL + `/images/unCheck.png`}
-              width="17"
-              height="17"
-              alt="AppearImg"
-            />
-          ) : (
-            <StChallengeImg
-              onClick={checkState}
-              src={process.env.PUBLIC_URL + `/images/check.png`}
-              width="17"
-              height="17"
-              alt="AppearImg"
-            />
-          )}
-          <StChallengeWord>도전완료 가리기</StChallengeWord>
-        </StChallengeWrap>
-        <StToggleImgWrap>
-          {/* 최신순 클릭시 아래에 정렬 bar 나옴 */}
-          <StToggle onClick={toggleSortPopUp}>{sortState}</StToggle>
-          <StToggleImg
-            onClick={toggleSortPopUp}
-            src={process.env.PUBLIC_URL + `/images/Toggle.png`}
-            width="12"
-            height="6"
-            alt="ToggleImg"
-          />
-        </StToggleImgWrap>
-      </StTopWrap>
-      <StTodayMyCardWrap>
-        {checkOn === true
-          ? //isChallenged가 true이면 화면에 띄우면 안된다.
-            //아래식이 isChallenged:true를 가지고있다를  어떻게 표현해야하는가
-            feedCard
-              ?.filter((elem) => elem.isChallenged === false)
-              .map((it, idx) => (
-                <ChallengeCard
-                  id={it.todoId}
-                  data={it}
-                  key={idx}
-                ></ChallengeCard>
-              ))
-          : feedCard?.map((it, idx) => (
-              <ChallengeCard id={it.todoId} data={it} key={idx}>
-                ?
-              </ChallengeCard>
-            ))}
-        <div className="hi" style={{ height: 80 }}></div>
-      </StTodayMyCardWrap>
-      <StSelectMbti onClick={moveToSelectMBTI}>MBTI 선택</StSelectMbti>
-    </StTotalWrap>
+      {loading === true ? <LoadingContainer /> : <></>}
+
+      <StTotalWrap>
+        {selectSort === true ? (
+          <StShadowBackgroundDiv onClick={toggleSortPopUp}>
+            <StPopupBox>
+              <StSlideDiv />
+              <StSort>
+                <StDate
+                  style={{
+                    color: sortState === "최신순" ? "#ff6d53" : "#8d8d8d",
+                  }}
+                  onClick={sortDate}
+                >
+                  최신순
+                </StDate>
+                <StDateLine />
+                <StComment
+                  style={{
+                    color: sortState === "댓글순" ? "#ff6d53" : "#8d8d8d",
+                  }}
+                  onClick={sortComment}
+                >
+                  댓글순
+                </StComment>
+                <StCommentLine />
+                <StChallenge
+                  style={{
+                    color: sortState === "도전순" ? "#ff6d53" : "#8d8d8d",
+                  }}
+                  onClick={sortChallenge}
+                >
+                  도전순
+                </StChallenge>
+                <StChallengeLine />
+                <StCommonBar />
+              </StSort>
+            </StPopupBox>
+          </StShadowBackgroundDiv>
+        ) : (
+          <></>
+        )}
+
+        <StTopWrap>
+          <StSearchBarBox>
+            <form onSubmit={searchData}>
+              <StInput
+                placeholder="검색"
+                onChange={inputSearch}
+                value={inputContext}
+                maxLength={20}
+              />
+              <StSearchBtn type="submit">
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  style={{ width: "23px", height: "23px", color: "#919191" }}
+                />
+              </StSearchBtn>
+            </form>
+          </StSearchBarBox>
+
+          <StWrap>
+            <StChallengeWrap>
+              {/* 거짓이면 체크안한거 참이면 체크한거 */}
+              {checkOn === false ? (
+                <StChallengeImg
+                  onClick={checkState}
+                  src={process.env.PUBLIC_URL + `/images/unCheck.png`}
+                  width="17"
+                  height="17"
+                  alt="AppearImg"
+                />
+              ) : (
+                <StChallengeImg
+                  onClick={checkState}
+                  src={process.env.PUBLIC_URL + `/images/check.png`}
+                  width="17"
+                  height="17"
+                  alt="AppearImg"
+                />
+              )}
+              <StChallengeWord>도전완료 가리기</StChallengeWord>
+            </StChallengeWrap>
+            <StToggleImgWrap>
+              {/* 최신순 클릭시 아래에 정렬 bar 나옴 */}
+              <StToggle onClick={toggleSortPopUp}>{sortState}</StToggle>
+              <StToggleImg
+                onClick={toggleSortPopUp}
+                src={process.env.PUBLIC_URL + `/images/Toggle.png`}
+                width="12"
+                height="6"
+                alt="ToggleImg"
+              />
+            </StToggleImgWrap>
+          </StWrap>
+        </StTopWrap>
+
+        <>
+          {searchList.length === 0 ? (
+            <StTodayMyCardWrap>
+              {checkOn === true
+                ? //isChallenged가 true이면 화면에 띄우면 안된다.
+                  //아래식이 isChallenged:true를 가지고있다를  어떻게 표현해야하는가
+                  feedCard
+                    ?.filter((elem) => elem.isChallenged === false)
+                    .map((it, idx) => (
+                      <ChallengeCard id={it.todoId} data={it} key={idx} />
+                    ))
+                : feedCard?.map((it, idx) => (
+                    <ChallengeCard id={it.todoId} data={it} key={idx} />
+                  ))}
+              <div className="hi" style={{ height: 80 }}></div>
+            </StTodayMyCardWrap>
+          ) : 
+          <StTodayMyCardWrap>
+          {searchList.map((x) => x.todoInfo)}
+          </StTodayMyCardWrap>
+                }
+        </>
+        <StSelectMbti onClick={moveToSelectMBTI}>MBTI 선택</StSelectMbti>
+      </StTotalWrap>
     </>
   );
 }
@@ -285,16 +337,20 @@ const StTotalWrap = styled.div`
 `;
 const StTopWrap = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   padding-top: 30px;
-  padding-bottom: 15px;
+  /* padding-bottom: 10px; */
   margin: 45px 0px 0px;
   background-color: #edecec;
   width: 500px;
   position: fixed;
   @media screen and (max-width: 500px) {
-    width: 360px
+    width: 360px;
   }
+`;
+const StWrap = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 const StChallengeWrap = styled.div`
   display: flex;
@@ -315,8 +371,8 @@ const StChallengeWord = styled.div`
   color: #000000;
   @media screen and (max-width: 500px) {
     align-items: center;
-    width: 175px;
-    margin:0px;
+    width: 185px;
+    margin: 0px;
   }
 `;
 const StToggleImgWrap = styled.div`
@@ -324,7 +380,7 @@ const StToggleImgWrap = styled.div`
   @media screen and (max-width: 500px) {
     align-items: center;
     width: 100%;
-    margin:0px;
+    margin: 0px;
   }
 `;
 const StToggle = styled.div`
@@ -339,7 +395,7 @@ const StToggle = styled.div`
   @media screen and (max-width: 500px) {
     align-items: center;
     width: 100.09px;
-    margin:0px 8px 0px 0px;
+    margin: 0px 8px 0px 0px;
     text-align: end;
   }
 `;
@@ -350,7 +406,7 @@ const StToggleImg = styled.img`
 `;
 const StTodayMyCardWrap = styled.div`
   flex-direction: column;
-  margin-top: 115px;
+  margin-top: 153px;
 `;
 const StSelectMbti = styled.button`
   display: flex;
@@ -373,7 +429,7 @@ const StSelectMbti = styled.button`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   @media screen and (max-width: 500px) {
     width: 144px;
-    margin-left:108px;
+    margin-left: 108px;
   }
 `;
 const StShadowBackgroundDiv = styled.div`
@@ -449,5 +505,70 @@ const StCommonBar = styled.div`
   left: calc(50% - 178.23px / 2 - 1.33px);
   background: #000000;
   border-radius: 133.005px;
+`;
+
+const StSearchBarBox = styled.div`
+  /* background-color: blue; */
+  display: flex;
+  /* position: relative; */
+  align-items: center;
+  opacity: 1;
+  width: 450px;
+  height: 55px;
+  margin: auto;
+  /* border: 1px solid #919191;
+  border-radius: 6px; */
+
+  @media only screen and (max-width: 500px) {
+    width: 330px;
+    margin-left: 15px;
+    margin-right: 15px;
+  }
+`;
+
+const StInput = styled.input`
+  /* background-color: burlywood; */
+  padding-left: 20px;
+  border: 1px solid #919191;
+  border-radius: 6px;
+  font-size: 18px;
+  width: 430px;
+  height: 50px;
+  z-index: -1;
+  outline: none;
+  /* margin-top:30px; */
+  overflow: auto; //검색어가 길어졌을때 오른쪽으로 자연스럽게 검색되도록 하기 위해
+  ::placeholder {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 18px;
+  }
+`;
+const StSearchBtn = styled.button`
+  /* background-color: blue; */
+  background: none;
+  margin-right: 20px;
+  border: none;
+  text-align: center;
+  outline: none;
+  cursor: pointer;
+  width: 33px;
+  height: 33px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 0;
+  transform: translateX(-60%) translateY(-130%);
+  @media screen and (max-width: 500px) {
+    transform: translateX(-20%) translateY(-130%);
+  }
+`;
+
+const StTest = styled.div`
+  background-color: red;
+  width: 500px;
+  height: 200px;
+  padding-top: 200px;
 `;
 export default FeedPageContainer;
